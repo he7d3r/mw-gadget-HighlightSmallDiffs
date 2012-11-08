52129fdc31f211ff6eac4dd5ce633e77596efafe
Helder <he7d3r@gmail.com>
2012-11-08 18:06:47 +0000

    Highlight any diffs which are too small
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/HighlightSmallDiffs.js
@@ -0,0 +1,22 @@
+/**
+ * Highlight any diffs which are too small
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/HighlightSmallDiffs.js]] ([[File:User:Helder.wiki/Tools/HighlightSmallDiffs.js]])
+ */
+/*jslint browser: true, white: true*/
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
+if( mw.util.getParamValue('diff') !== null ){
+	$(function(){
+		$('.diffchange-inline').filter(function(){
+			return $( this ).text().length < 4;
+		}).css( {
+			background: '#FF7B7B',
+			padding: '0 0.2em'
+		} );
+	});
+}
+
+}( mediaWiki, jQuery ) );
