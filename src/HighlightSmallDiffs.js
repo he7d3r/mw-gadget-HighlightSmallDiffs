2f2db69e8d2a17eecfeafc303bd2ae0c1acbf10e
Helder <he7d3r@gmail.com>
2014-09-27 20:49:00 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/HighlightSmallDiffs.js
+++ src/HighlightSmallDiffs.js
@@ -4,23 +4,22 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-if( mw.util.getParamValue( 'diff' ) !== null ){
-	$( function(){
-		var whiteSpaceChanges = function(){
-			return $.trim( $( this ).text() ).length === 0;
-		};
-		$( '.diff-deletedline .diffchange-inline' ).filter( whiteSpaceChanges ).css( {
-			background: '#FF7B7B',
-			padding: '0 0.2em'
+	if ( mw.util.getParamValue( 'diff' ) !== null ) {
+		$( function () {
+			var whiteSpaceChanges = function () {
+				return $.trim( $( this ).text() ).length === 0;
+			};
+			$( '.diff-deletedline .diffchange-inline' ).filter( whiteSpaceChanges ).css( {
+				background: '#FF7B7B',
+				padding: '0 0.2em'
+			} );
+			$( '.diff-addedline .diffchange-inline' ).filter( whiteSpaceChanges ).css( {
+				background: '#7BFF7B',
+				padding: '0 0.2em'
+			} );
 		} );
-		$( '.diff-addedline .diffchange-inline' ).filter( whiteSpaceChanges ).css( {
-			background: '#7BFF7B',
-			padding: '0 0.2em'
-		} );
-		
-	} );
-}
+	}
 
 }( mediaWiki, jQuery ) );
